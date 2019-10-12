33d60c0d9a8083ab2aa1e3a7189aaa03dcdc1c75
srmonahan <monahas4@tcnj.edu>
2019-10-11 21:01:29 -0400

    HW3 #1 commit

--- HWs/HW3/calculate_LCM.m
+++ HWs/HW3/calculate_LCM.m
@@ -1,14 +1,14 @@
 function lcm_val = calculate_LCM(x, y)
 
-
 xvec = [x:x:x*y];
-yvec = [y:y:x*y];
-n = 1;
+% vector of all of the multiples of x until x*y
 
 for i=1:length(xvec)
    
         if mod(xvec(i), y)==0
+        % finds the multiple of x that y divides
             xvec(i);
         end
 end
 xvec(i)
+% prints out the first multiple of x that y divides
